d6a9f755c6adc7173cb4e13ed5858efc82b9acb4
sreesau <sreesau6@gmail.com>
2023-05-29 00:10:06 +0530

    program to get the 4th element and 4th element from last of a tuple.

---
+++ Answer#8.py
@@ -0,0 +1,7 @@
+#Write a Python program to get the 4th element and 4th element from last of a tuple.
+tuple_collection=("apple","orange","tomato",4,5,6,"lotus","rose","lilly","jasmine")
+i=tuple_collection[4]
+j=tuple_collection[-4]
+print("Tuple:",tuple_collection)
+print("4th element of given tuple is:",i)
+print("4th element from last of given tuple is:",j)
