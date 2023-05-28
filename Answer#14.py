18991a00ce2ac3347bf5ecb76a9ab08969fecf28
sreesau <sreesau6@gmail.com>
2023-05-29 00:29:29 +0530

    program to remove an item from a set if it is present in the set.

---
+++ Answer#14.py
@@ -0,0 +1,5 @@
+#Write a Python program to remove an item from a set if it is present in the set.
+set={"apple","banana","pear","mango","orange","guava"}
+print(set)
+set.remove("guava")
+print(set)
