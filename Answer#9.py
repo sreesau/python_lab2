6be54e75cbba3202e8baf02660c395467b95c8ce
sreesau <sreesau6@gmail.com>
2023-05-29 00:14:39 +0530

    program to find the index of an item of a tuple.

---
+++ Answer#9.py
@@ -0,0 +1,4 @@
+#Write a Python program to find the index of an item of a tuple.
+tuple_collection=("apple","orange","tomato",4,5,6,"lotus","rose","lilly","jasmine")
+for i in tuple_collection:
+       print("Index of",i,"is",tuple_collection.index(i))
