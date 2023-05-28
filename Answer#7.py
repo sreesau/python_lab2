83bc0dffdf4b9cb16d91beb0c20e05e3dd3e77a9
sreesau <sreesau6@gmail.com>
2023-05-29 00:07:36 +0530

    program to get the key, value and item in a dictionary

---
+++ Answer#7.py
@@ -0,0 +1,4 @@
+#Write a Python program to get the key, value and item in a dictionary
+dict_num = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+for key,value in dict_num.items():
+    print(key,value)
